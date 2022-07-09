f329d62b7df89ad4ca34667e7e569181f1a8aa39
Andrew Cassetti <andrew@concorde.studio>
2022-07-09 19:30:19 -0400

    created factories

---
+++ src/services/chain-sim/contract-templates/scratchpad.sol
@@ -0,0 +1,20 @@
+        // // TO-DO: Move to params?
+        // depsList[0] = Asset(
+        //     "b64jseval",
+        //     "compressorGlobalB64"
+        // );
+        // depsList[1] = Asset(
+        //     "gzhexjs",
+        //     "p5gzhex"
+        // );
+        // depsList[2] = Asset(
+        //     "rawjs",
+        //     "p5setup"
+        // );
+
+        // depsCount = 3;
+
+        // assetList[0] = Asset(
+        //     "rawjs",
+        //     "draw"
+        // );
