f82b5cfc8659a6dd3ca170d89794d743c1505f1d
Andrew Cassetti <andrew@concorde.studio>
2022-08-09 13:19:47 -0400

    updated contracts

--- src/services/chain-sim/contracts/CloneFactory.sol
+++ src/services/chain-sim/contracts/CloneFactory.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 contract CloneFactory {
   function createClone(address target) internal returns (address result) {
     bytes20 targetBytes = bytes20(target);
