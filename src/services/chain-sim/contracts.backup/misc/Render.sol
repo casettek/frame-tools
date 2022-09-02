157b85b46b61911585f877dc44d88c83140d7e93
Andrew Cassetti <andrew@concorde.studio>
2022-09-02 00:24:50 -0400

    checkpoint

---
+++ src/services/chain-sim/contracts.backup/misc/Render.sol
@@ -0,0 +1,1 @@
+contract Renderer { struct Asset { string assetType; string key; string[2] wrapper; uint256 maxPageNumber; }  string[2] public renderWrapper = ['RENDER_WRAPPER']; mapping(uint256 => Asset) public assetList; uint256 public renderPagesCount; mapping(uint256 => uint256[4]) public renderIndex; constructor() {}}
