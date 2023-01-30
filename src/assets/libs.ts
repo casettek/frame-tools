6d1aa2ca826718866e1a4cb8d9b611fb4ab7a61a
cncrde <andrew@concorde.studio>
2023-01-30 15:21:16 -0500

    update files

--- src/assets/libs.ts
+++ src/assets/libs.ts
@@ -1,6 +1,6 @@
 const fs = require("fs");
 export const importIds = {
-  p5gz: "p5.min.js.gz",
+  p5gz: "p5-v1.5.0.min.js.gz",
   gunzip: "gunzipScripts-0.0.1.js",
 };
 
@@ -9,7 +9,9 @@ const { p5gz, gunzip } = importIds;
 export const wrappers = {};
 
 export const importData = {
-  [p5gz]: fs.readFileSync(__dirname + "/p5.min.js.gz").toString("base64"),
+  [p5gz]: fs
+    .readFileSync(__dirname + "/p5-v1.5.0.min.js.gz")
+    .toString("base64"),
   [gunzip]: fs
     .readFileSync(__dirname + "/gunzipScripts-0.0.1.js")
     .toString("base64"),
